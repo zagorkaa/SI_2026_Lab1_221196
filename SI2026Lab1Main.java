fdfdd1bb2e0b2f7ac33c7618e4fdb33512816dc7
Zagorka <anevskaz17@gmail.com>
2026-03-27 23:12:18 +0100

    Implement genre report

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -96,6 +96,11 @@ class Library {
 
     // TODO: Implement in branch feature-genre-report
     public void printBooksByGenre(String genre) {
+        for (Book book : books) {
+            if (book.getGenre().equalsIgnoreCase(genre)) {
+                System.out.println(book);
+            }
+        }
     }
 
     public int countAvailableBooks() {
@@ -130,5 +135,8 @@ public class SI2026Lab1Main {
 
         System.out.println(library.searchBookByTitle("Clean Code"));
         System.out.println(library.searchBookByTitle("Unknown Book"));
+
+        library.printBooksByGenre("Programming");
+        library.printBooksByGenre("Fantasy");
     }
 }
